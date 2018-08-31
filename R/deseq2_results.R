9edb95b9e6c6e4228e083ea4c8186b1e4ec5d645
travis-m-blimkie <travis.m.blimkie@gmail.com>
2018-08-31 14:47:17 -0700

    Moved functions to individual files

---
+++ R/deseq2_results.R
@@ -0,0 +1,10 @@
+# deseq2_results ----------------------------------------------------------
+
+# Takes a DESeq results object as input (i.e. must have defined contrast)
+# Adds two new columns and filters based on padj and ABSLFC
+
+deseq2_results <- function(result, pAdj = 0.05, fc = 1.5) {
+    result$ABSLFC <- abs(result$log2FoldChange)
+    result$FC <- (sign(result$log2FoldChange)) * (2 ^ (result$ABSLFC))
+    result <- subset(result, padj <= pAdj & ABSLFC >= log2(fc))
+}
