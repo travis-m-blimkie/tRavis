1801c208303d12246bc7e56c5c73a978ad74c261
travis-m-blimkie <travis.m.blimkie@gmail.com>
2018-09-26 15:53:23 -0700

    Changes to deseq2_results function.

--- R/deseq2_results.R
+++ R/deseq2_results.R
@@ -4,9 +4,9 @@
 # Adds two new columns and filters based on padj and ABSLFC
 
 deseq2_results <- function(result, pAdj = 0.05, fc = 1.5) {
-    result$ABSLFC <- abs(result$log2FoldChange)
-    result$FC <- (sign(result$log2FoldChange)) * (2 ^ (result$ABSLFC))
-    result <- subset(result, padj <= pAdj & ABSLFC >= log2(fc))
-    result$gene <- rownames(result)
-    rownames(result) <- NULL
+  result$ABSLFC <- abs(result$log2FoldChange)
+  result$FC <- (sign(result$log2FoldChange)) * (2 ^ (result$ABSLFC))
+  result <- subset(result, padj <= pAdj & ABSLFC >= log2(fc))
+  colnames(result)[colnames(result) == "row"] <- "gene"
+  return(result)
 }
