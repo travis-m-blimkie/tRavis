932d8e0bd769b82a723dc814515028d5dabd9401
travis-m-blimkie <travis.m.blimkie@gmail.com>
2018-09-26 16:08:28 -0700

    Changes to deseq2_results() to include check for correct input type.

--- R/deseq2_results.R
+++ R/deseq2_results.R
@@ -4,6 +4,7 @@
 # Adds two new columns and filters based on padj and ABSLFC
 
 deseq2_results <- function(result, pAdj = 0.05, fc = 1.5) {
+  if (is.data.frame(result) != T) stop("Did you call results() with the 'tidy = T' option?")
   result$ABSLFC <- abs(result$log2FoldChange)
   result$FC <- (sign(result$log2FoldChange)) * (2 ^ (result$ABSLFC))
   result <- subset(result, padj <= pAdj & ABSLFC >= log2(fc))
