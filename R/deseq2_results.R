ef9df6477cc6274a251ceff1ca69add1a7e4f828
travis-m-blimkie <travis.m.blimkie@gmail.com>
2018-09-26 15:45:25 -0700

    Changes to deseq2_results function.

--- R/deseq2_results.R
+++ R/deseq2_results.R
@@ -7,4 +7,6 @@ deseq2_results <- function(result, pAdj = 0.05, fc = 1.5) {
     result$ABSLFC <- abs(result$log2FoldChange)
     result$FC <- (sign(result$log2FoldChange)) * (2 ^ (result$ABSLFC))
     result <- subset(result, padj <= pAdj & ABSLFC >= log2(fc))
+    result$gene <- rownames(result)
+    rownames(result) <- NULL
 }
