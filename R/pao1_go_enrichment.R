246c9e8dd419ab3a15af988b82e345b1b9858f1e
travis-m-blimkie <travis.m.blimkie@gmail.com>
2018-09-28 15:41:58 -0700

    New function and related support.

---
+++ R/pao1_go_enrichment.R
@@ -0,0 +1,63 @@
+# Defines a function that performs GO term enrichment given an input list of genes
+# p-value can be set as desired, with a default of 0.05
+
+pao1_go_enrichment <- function(GOI, pval = 0.05) {
+
+  require(tidyverse)
+  
+  # Read in GO list for testing ---------------------------------------------
+
+  go_list <- readRDS("/home/travisblimkie/go_list.rds")
+  
+  
+  # List of overlap size ----------------------------------------------------
+  
+  overlap_list <- map(.x = go_list, function(x) 
+    as.numeric(length(intersect(x, GOI)))
+  )
+  
+  enrichment_lengths <- map(.x = go_list, function(x) length(x))                          
+  
+  query_length <- as.numeric(length(GOI))
+  
+  total_genes <- 5688
+  
+  
+  # Construct matrices ------------------------------------------------------
+  
+  matrices <- map2(.x = overlap_list, .y = enrichment_lengths, function(x, y) 
+    enrichment_matrix <- matrix(c(
+      x, 
+      as.numeric(y - x), 
+      as.numeric(query_length - x), 
+      as.numeric(total_genes - (y - x))
+    ), nrow = 2, ncol = 2
+    
+    )
+  )
+  
+  
+  # Run Fishers test on each ------------------------------------------------
+  
+  fishers_result <- map(.x = matrices, function(x) 
+    fisher.test(x, alternative = "greater")$p.value
+  )
+  
+  
+  # Create result table -----------------------------------------------------
+  
+  result_table <- tibble(
+    term = names(fishers_result), 
+    pvalue = as.numeric(fishers_result[names(fishers_result)])
+  )
+  
+  
+  # Filter the result -------------------------------------------------------
+  
+  filtered_result <- result_table %>% 
+    filter(pvalue <= 0.05) %>% 
+    arrange(pvalue)
+  
+  return(filtered_result)
+  
+}
--- README.md
+++ README.md
@@ -9,4 +9,5 @@ Install via `devtools::install_github()` function.
 	The call to `results()` which feeds to this function must include the `tidy = T` option. 
 - **biomart_table** Creates a table from Biomart for converting between different ID types. Currently supports Ensembl, Entrez, and HGNC. 
 - **pao1_enrichment** Tests for enrichment of a signature set of genes in a list of genes of interest, using Fisher's Exact Test.
+- **pao1_go_enrichment** Test for enrichment of a set of genes of interest against all GO terms for *P. aeurginosa*. 
 
