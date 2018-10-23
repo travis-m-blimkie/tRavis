d8982f23bcdce7f6090368ea4569dcb50e5c4ff2
travis-m-blimkie <travis.m.blimkie@gmail.com>
2018-10-23 14:56:17 -0700

    Updates to pao1_enrichment function. Added man pages for remaining functions.

--- R/pao1_enrichment.R
+++ R/pao1_enrichment.R
@@ -2,7 +2,7 @@
 #  comparing to some specified background. Uses Fisher's Exact Test for p-value. 
 
 
-pao1_enrichment <- function(query_genes, enrichment_set, total_genes = 5688) {
+pao1_enrichment <- function(query_genes, enrichment_set) {
   require(tidyverse)
 
   # Calculate overlap between the query list and enrichment set
@@ -13,7 +13,7 @@ pao1_enrichment <- function(query_genes, enrichment_set, total_genes = 5688) {
     num_overlap,
     as.numeric(length(enrichment_set) - num_overlap),
     as.numeric(length(query_genes) - num_overlap),
-    as.numeric(total_genes - (length(enrichment_set) - num_overlap))
+    as.numeric(5688 - (length(enrichment_set) - num_overlap))
   ), nrow = 2, ncol = 2)
 
   # Run and return Fisher's Exact test on the matrix
