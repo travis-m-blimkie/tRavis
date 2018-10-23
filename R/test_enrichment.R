8062f1fecb2c9087d0a60148432d8f75a47ac5d4
travis-m-blimkie <travis.m.blimkie@gmail.com>
2018-10-23 15:22:21 -0700

    Changed name of function.

--- R/test_enrichment.R
+++ R/test_enrichment.R
@@ -2,20 +2,27 @@
 #  comparing to some specified background. Uses Fisher's Exact Test for p-value. 
 
 
-pao1_enrichment <- function(query_genes, enrichment_set) {
+test_enrichment <- function(query_genes, enrichment_set, total_genes) {
+  
   require(tidyverse)
-
-  # Calculate overlap between the query list and enrichment set
+  
+  
+  # Calculate overlap between the query list and enrichment set -------------
+  
   num_overlap <- as.numeric(length(intersect(query_genes, enrichment_set)))
   
-  # Construct the matrix to be used for the test
+
+  # Construct the matrix to be used for the test ----------------------------
+  
   enrichment_matrix <- matrix(c(
     num_overlap,
     as.numeric(length(enrichment_set) - num_overlap),
     as.numeric(length(query_genes) - num_overlap),
-    as.numeric(5688 - (length(enrichment_set) - num_overlap))
+    as.numeric(total_genes - (length(enrichment_set) - num_overlap))
   ), nrow = 2, ncol = 2)
 
-  # Run and return Fisher's Exact test on the matrix
+
+  # Run and return Fisher's Exact test on the matrix ------------------------
+  
   return(fisher.test(enrichment_matrix, alternative = "greater"))
 }
--- README.md
+++ README.md
@@ -8,6 +8,6 @@ Install via `devtools::install_github()` function.
 - **deseq2_results** Adds two columns to a DESeq results object and filters based on p-value and fold change. 
 	The call to `deseq2::results()` which feeds to this function must include the `tidy = T` option. 
 - **biomart_table** Creates a table from Biomart for converting between different ID types. Currently supports Ensembl, Entrez, and HGNC. 
-- **pao1_enrichment** Tests for enrichment of a signature set of genes in a list of genes of interest, using Fisher's Exact Test.
+- **test_enrichment** Tests for enrichment of a signature set of genes in a list of genes of interest, using Fisher's Exact Test.
 - **pao1_go_enrichment** Test for enrichment of a set of genes of interest against all GO terms for *P. aeruginosa*. 
 
