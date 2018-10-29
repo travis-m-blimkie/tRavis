486b863296428fca15a589e7709532881cf0a773
Travis <travis.m.blimkie@gmail.com>
2018-10-29 16:12:29 -0700

    Changes to man pages.
    Changed number of total genes for PAO1 in tr_go_enrichment.
    Work on pathway enrichment function.

--- R/pao1_pe.R
+++ R/pao1_pe.R
@@ -3,7 +3,7 @@
 #' Currently set up as a simple Fisher's Test of overlapping genes
 
 
-pao1_pe <- function(GOI, pvalue = 0.05) {
+pao1_pe <- function(GOI, strain = "", pvalue = 0.05) {
 
   require(tidyverse)
 
@@ -11,9 +11,31 @@ pao1_pe <- function(GOI, pvalue = 0.05) {
   if (is.character(GOI) != T) stop("Did you provide your genes as a character vector?")
 
   # Download the pathway annotations from my Github
-  pao1_anno <- read_tsv(
-    "https://raw.githubusercontent.com/travis-m-blimkie/misc_files/master/pao1_annotations_tidy.tsv"
+  if (strain == "PAO1" ) {
+    annotations <- read_tsv(
+      "https://raw.githubusercontent.com/travis-m-blimkie/misc_files/master/Pathway_Annotations/pathway_annotations_PAO1.tsv"
+      )
+    total_genes = 5700
+  } else if (strain == "PA14") {
+    annotations <- read_tsv(
+      "https://raw.githubusercontent.com/travis-m-blimkie/misc_files/master/Pathway_Annotations/pathway_annotations_PA14.tsv"
     )
+    total_genes <- 5983
+  } else if (strain == "LESB58") {
+    annotations <- read_tsv(
+      "https://raw.githubusercontent.com/travis-m-blimkie/misc_files/master/Pathway_Annotations/pathway_annotations_LESB58.tsv"
+    )
+    total_genes <- 6028
+  } else if (strain == "") {
+    stop("Please select one of the following supported strains: 'PAO1', 'PA14', or 'LESB58'")
+  }
+
+  annotations <- list(
+    KEGG = filter(annotations, `Pathway Database`)
+  )
+
+
+
 
 }
 
