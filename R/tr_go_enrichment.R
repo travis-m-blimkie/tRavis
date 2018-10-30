2a79e28a71a9b9792e5e7e7ff6136aa71d853ecb
Travis <travis.m.blimkie@gmail.com>
2018-10-30 09:27:05 -0700

    Fixed error in tr_go_enrichment.

--- R/tr_go_enrichment.R
+++ R/tr_go_enrichment.R
@@ -20,7 +20,7 @@ tr_go_enrichment <- function(GOI, pval = 0.05, strain = "", GO_class = "all") {
 
   if (condition) {
 
-  }f (strain == "PAO1") {
+  } if (strain == "PAO1") {
     go_table_all <- suppressMessages(read_tsv(
       "https://raw.githubusercontent.com/travis-m-blimkie/tRavis/master/gene_ontology_terms_PAO1.txt"
     ))
