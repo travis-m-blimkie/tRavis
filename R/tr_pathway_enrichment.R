1ae86ad799c8fdc3d1389d31c532ae7b17d878a7
Travis <travis.m.blimkie@gmail.com>
2018-11-02 09:46:15 -0700

    Working on pathway enrichment.

--- R/tr_pathway_enrichment.R
+++ R/tr_pathway_enrichment.R
@@ -15,24 +15,20 @@ tr_pathway_enrichment <- function(GOI, strain = "", pvalue = 0.05) {
     annotations <- read_tsv(
       "https://raw.githubusercontent.com/travis-m-blimkie/misc_files/master/Pathway_Annotations/pathway_annotations_PAO1.tsv"
       )
-    total_genes = 5700
+    total_genes <- nrow(distinct(annotations, Locus_Tag)) %>% as.numeric()
   } else if (strain == "PA14") {
     annotations <- read_tsv(
       "https://raw.githubusercontent.com/travis-m-blimkie/misc_files/master/Pathway_Annotations/pathway_annotations_PA14.tsv"
     )
-    total_genes <- 5983
+    total_genes <- nrow(distinct(annotations, Locus_Tag)) %>% as.numeric()
   } else if (strain == "LESB58") {
     annotations <- read_tsv(
       "https://raw.githubusercontent.com/travis-m-blimkie/misc_files/master/Pathway_Annotations/pathway_annotations_LESB58.tsv"
     )
-    total_genes <- 6028
+    total_genes <- nrow(distinct(annotations, Locus_Tag)) %>% as.numeric()
   } else if (strain == "") {
     stop("Please select one of the following supported strains: 'PAO1', 'PA14', or 'LESB58'")
   }
-
-  annotations <- list(
-    KEGG = filter(annotations, `Pathway Database`)
-  )
 
 
 
