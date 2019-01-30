d470ca3d9e094c66a78b38ce8b88bb3e843cb7c7
Travis <travis.m.blimkie@gmail.com>
2019-01-30 13:39:20 -0800

    New function for genebody plotting.

---
+++ R/tr_genebody_plotly.R
@@ -0,0 +1,44 @@
+
+# Function to read in QoRTs results and make a combined plotly of gene body
+# results for all samples
+
+
+tr_genebody_plotly <- function(qorts_dir) {
+
+  # Load libraries
+  require(QoRTs)
+  require(dplyr)
+  require(plotly)
+
+
+  # Get sample names based on directory names
+  sample_ids <- list.dirs(qorts_dir, full.names = F, recursive = F)
+
+
+  # Read in all the results using QoRTs function
+  qorts_results <- read.qc.results.data(infile.dir = qorts_dir, decoder = sample_ids)
+
+
+  # Pull out the gene body coverage results
+  genebodies <- bind_rows(qorts_results@qc.data[["geneBodyCoverage.pct"]], .id = "SampleName")
+
+
+  # Plotly of gene body coverage for all samples
+  genebody_plot <- plot_ly(group_by(genebodies, SampleName),
+    x = ~QUANTILE,
+    y = ~X2.upperMidQuartile,
+    name = "Upper_Mid_Quartile",
+    type = "scatter",
+    mode = "lines",
+    text = ~SampleName,
+    alpha = 0.3
+  ) %>%
+    layout(
+      xaxis = list(title = "Percentile of Gene Body (5'->3')"),
+      yaxis = list(title = "Proportion of Reads"),
+      title = "Gene Body Coverage: Upper-Middle Quartile"
+    )
+
+  return(genebody_plot)
+
+}
