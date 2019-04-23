f5d64926aba72215ae103dda05181e93ca1a7d52
Travis <travis.m.blimkie@gmail.com>
2019-04-23 09:52:29 -0700

    Added heatmap QoL function.

---
+++ R/tr_heatmap.R
@@ -0,0 +1,25 @@
+
+# tRavis::tr_heatmap() ----------------------------------------------------
+
+tr_heatmap <- function(x) {
+
+  require(tidyverse)
+  require(pheatmap)
+  require(RColorBrewer)
+
+
+  heatmap_colour <- rev(colorRampPalette(brewer.pal(9, "RdBu"))(100))
+
+  heatmap_matrix <- assay(x) %>%
+    t() %>%
+    dist() %>%
+    as.matrix()
+
+  rownames(heatmap_matrix) <- colnames(heatmap_matrix)
+
+
+  pheatmap(heatmap_matrix,
+           color = heatmap_colour,
+           border_color = NA)
+
+}
