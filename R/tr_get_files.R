7c6cd61c74f1e86f88a8fb231c1dd738e7d11033
Travis <travis.m.blimkie@gmail.com>
2020-01-10 14:41:15 -0800

    Removed uneeded map() statements, added more info to description and arguments.

--- R/tr_get_files.R
+++ R/tr_get_files.R
@@ -2,8 +2,8 @@
 #'
 #' @param folder Directory containing files of interest.
 #' @param pattern Optional pattern to use in file searching.
-#' @param date Do file names contain a date which should be removed? Should be
-#'   of the format "YYYYMMDD"
+#' @param date Do file names contain a date which should be removed? Must be of
+#'   the format "YYYYMMDD".
 #' @param removeString Optional string which can be removed from file names when
 #'   creating names for the list.
 #'
@@ -11,13 +11,16 @@
 #'
 #' @export
 #'
+#' @import dplyr
 #' @import purrr
 #' @import stringr
 #'
 #' @description Function which creates a named list of files in a specified
-#'   directory. Names are trimmed versions of file names, contents of the list
-#'   are the file names themselves. Can be easily piped into
-#'   \code{purrr::map(~read.csv(.))} to create named list of data frames.
+#'   directory. The list names are trimmed versions of file names, while
+#'   contents of the list are the file names themselves. In this way, it can be
+#'   easily piped into \code{purrr::map(~read.csv(.))} to create named list of
+#'   data frames. Note this function will only find files with the extension
+#'   "csv", "tsv", or "txt".
 #'
 #' @references None.
 #'
@@ -30,6 +33,8 @@ tr_get_files <- function(folder, pattern = "", date = FALSE, removeString = NULL
   f_Files <- list.files(folder, pattern = pattern, full.names = TRUE) %>%
     grep("(csv|tsv|txt)$", ., value = TRUE)
 
+  # Provide a custom error message if no files are found, and remind the user
+  # that only certain file extensions are supported.
   if (length(f_Files) == 0) {
     stop(paste0(
       "No files found matching the specified pattern. Please note ",
@@ -42,24 +47,21 @@ tr_get_files <- function(folder, pattern = "", date = FALSE, removeString = NULL
   # extension from the end.
   f_Names <- list.files(folder, pattern = pattern) %>%
     grep("(csv|tsv|txt)$", ., value = TRUE) %>%
-    map(~str_remove(., pattern = "\\.(csv|tsv|txt)"))
+    str_remove(., pattern = "\\.(csv|tsv|txt)")
 
   # If specified, remove dates from the names for files.
   if (date == TRUE) {
-    f_Names <- f_Names %>%
-      map(~str_remove(., pattern = "_?[0-9]{8}"))
+    f_Names <- f_Names %>% str_remove(., pattern = "_?[0-9]{8}")
   }
 
   # Remove specified string if provided. Needs to be in a conditional, otherwise
   # `str_remove()` returns an error for trying to remove nothing/everything/
   # anything.
   if (!is.null(removeString)) {
-    f_Names <- f_Names %>% map(
-      ~str_remove_all(., pattern = removeString)
-    )
+    f_Names <- f_Names %>% str_remove_all(., pattern = removeString)
   }
 
   # Create and return output object
   f_Output <- set_names(f_Files, f_Names)
-  return(as.list(f_Output))
+  return(f_Output)
 }
