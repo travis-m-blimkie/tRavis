1d20e305805b707838d7522e60232e667964f287
Travis <travis.m.blimkie@gmail.com>
2020-02-14 09:37:05 -0800

    Added grid argument to theming function, updated man and version accordingly.

--- R/tr_theme.R
+++ R/tr_theme.R
@@ -2,6 +2,8 @@
 #'
 #' @param baseSize Base font size applied to all text in the plot
 #' @param baseFamily Base font type applies to all text in the plot
+#' @param grid Determines if grid lines should be drawn. Defaults to FALSE. When
+#'   TRUE, only "major" grid lines are included.
 #'
 #' @return Theme object
 #'
@@ -18,15 +20,29 @@
 #'
 #' @examples
 #' \dontrun{
-#'   ggplot(mtcars, aes(cyl, mpg)) + geom_point() + tr_theme()
+#'   ggplot(mtcars, aes(cyl, mpg)) + geom_point() + tr_theme(grid = TRUE)
 #' }
 #'
-tr_theme <- function(baseSize = 16, baseFamily = "") {
-  theme_light(base_size = baseSize, base_family = baseFamily) +
-    theme(
-      text = element_text(colour = "black"),
-      panel.grid.major = element_line("grey"),
-      panel.border = element_rect(colour = "black", size = 1)
-    )
+tr_theme <- function(baseSize = 16, baseFamily = "", grid = FALSE) {
+  if (grid) {
+    theme_light(base_size = baseSize, base_family = baseFamily) +
+      theme(
+        text = element_text(colour = "black"),
+        axis.text = element_text(colour = "black"),
+        axis.ticks = element_line(colour = "black"),
+        panel.grid.major = element_line(colour = "grey"),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black", size = 1)
+      )
+  } else {
+    theme_light(base_size = baseSize, base_family = baseFamily) +
+      theme(
+        text = element_text(colour = "black"),
+        axis.text = element_text(colour = "black"),
+        axis.ticks = element_line(colour = "black"),
+        panel.grid = element_blank(),
+        panel.border = element_rect(colour = "black", size = 1)
+      )
+  }
 }
 
