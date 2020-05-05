ad79a328bb413d50fba39c7ecd352108e058e9da
Travis M Blimkie <travis.m.blimkie@gmail.com>
2020-05-05 15:06:28 -0700

    Added function to display a simple message when the package is loaded.

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+
+.onAttach <- function(...) {
+  packageStartupMessage(paste0(
+    "Thanks for using tRavis!\n",
+    "If you encounter any bugs or problems, please submit an issue at the\n",
+    "Github page: https://github.com/travis-m-blimkie/tRavis/issues"
+  ))
+}
