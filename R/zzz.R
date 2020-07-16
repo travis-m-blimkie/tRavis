1f9a5eff129c2b7c1a3bce9448b6f0efae6c40fa
Travis M Blimkie <travis.m.blimkie@gmail.com>
2020-07-16 11:29:31 -0700

    Fixed problem with use of packageVersion in zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 
 .onAttach <- function(...) {
   packageStartupMessage(paste0(
-    "Thanks for using tRavis!\n",
+    "Thanks for using tRavis v", utils::packageVersion("tRavis"), "!\n",
     "If you encounter any bugs or problems, please submit an issue at the\n",
     "Github page: https://github.com/travis-m-blimkie/tRavis/issues"
   ))
