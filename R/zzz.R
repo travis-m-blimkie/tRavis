57cd421d519250d5b5b62924e9a8fe59105d692a
Travis M Blimkie <travis.m.blimkie@gmail.com>
2020-08-25 12:07:08 -0700

    Changed colour of startup message.

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 
 .onAttach <- function(...) {
 
-  packageStartupMessage(crayon::blue(paste0(
+  packageStartupMessage(crayon::white(paste0(
       "Thanks for using tRavis v", utils::packageVersion("tRavis"), "!\n",
       "If you encounter any bugs or problems, please submit an issue at the\n",
       "Github page: https://github.com/travis-m-blimkie/tRavis/issues"
