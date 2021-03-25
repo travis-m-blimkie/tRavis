03896265b9ca7574c72fb3aa23d5edbbffd7489e
Travis <travis.m.blimkie@gmail.com>
2021-03-25 11:52:33 -0700

    Removed colour spec from startup message, so it doesn't conflict with different themes.

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
 
 .onAttach <- function(...) {
 
-  packageStartupMessage(crayon::white(paste0(
+  packageStartupMessage(paste0(
       "Thanks for using tRavis v", utils::packageVersion("tRavis"), "!\n",
       "If you encounter any bugs or problems, please submit an issue at the\n",
       "Github page: https://github.com/travis-m-blimkie/tRavis/issues"
-  )))
+  ))
 }
