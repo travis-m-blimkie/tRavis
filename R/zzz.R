593a99c53d467e1fef59850c7a40194f2f9c6785
Travis <travis.m.blimkie@gmail.com>
2021-05-14 15:50:35 -0700

    Made startup message wrap to console width.

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,11 @@
 
 .onAttach <- function(...) {
 
-  packageStartupMessage(paste0(
-      "Thanks for using tRavis v", utils::packageVersion("tRavis"), "!\n",
-      "If you encounter any bugs or problems, please submit an issue at the\n",
+  packageStartupMessage(
+    paste0(
+      "Thanks for using tRavis v", utils::packageVersion("tRavis"), "! ",
+      "If you encounter any bugs or problems, please submit an issue at the ",
       "Github page: https://github.com/travis-m-blimkie/tRavis/issues"
-  ))
+    ) %>% stringr::str_wrap(width = getOption("width"))
+  )
 }
