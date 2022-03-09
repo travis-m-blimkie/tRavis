8574ddf5edc5eef98e06878b426e3f92e3c66e73
Travis <travis.m.blimkie@gmail.com>
2022-03-09 09:39:44 -0800

    Added names argument to tr_compare_lists
    Former-commit-id: 731a124b94e746052230b666f62740caeedccf01

--- R/tr_compare_lists.R
+++ R/tr_compare_lists.R
@@ -2,6 +2,8 @@
 #'
 #' @param x First vector to compare.
 #' @param y Second vector to compare.
+#' @param names Optional character vector, containing names of `x` and `y` to
+#'   apply to the output.
 #'
 #' @return A named list of the common and unique elements of x and y.
 #'
@@ -21,18 +23,27 @@
 #' \dontrun{
 #'   tr_compare_lists(
 #'     x = c(1, 2, 4, 5, 6),
-#'     y = c(2, 3, 6, 7)
+#'     y = c(2, 3, 6, 7),
+#'     names = c("A", "B")
 #'   )
 #' }
 #'
-tr_compare_lists <- function(x, y) {
+tr_compare_lists <- function(x, y, names = NULL) {
 
   if ((is.vector(x) & is.vector(y)) == FALSE) {
     stop("Wrong input type. Please ensure both 'x' and 'y' are vectors.")
   }
 
-  list(common   = intersect(x, y),
-       unique_x = setdiff(x, y),
-       unique_y = setdiff(y, x)
+  output_list <- list(
+    unique_x = setdiff(x, y),
+    common   = intersect(x, y),
+    unique_y = setdiff(y, x)
   )
+
+  if (!is.null(names)) {
+    names(output_list)[1] <- paste0("unique_", names[1])
+    names(output_list)[3] <- paste0("unique_", names[2])
+  }
+
+  return(output_list)
 }
