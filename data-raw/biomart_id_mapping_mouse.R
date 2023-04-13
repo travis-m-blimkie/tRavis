e89f4f8e6870a57e03c06d0ccb92f8e4408b95e2
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-04-13 10:02:21 -0700

    Changes to scripts generating biomart ID mapping tables.

--- data-raw/biomart_id_mapping_mouse.R
+++ data-raw/biomart_id_mapping_mouse.R
@@ -3,22 +3,20 @@ library(biomaRt)
 library(tidyverse)
 
 # Use `biomaRt::getBM()` to create the conversion table
-biomart_id_mapping_mouse_1 <- getBM(
+biomart_id_mapping_mouse_0 <- getBM(
   attributes = c("ensembl_gene_id", "mgi_symbol", "entrezgene_id", "description"),
   mart = useMart("ensembl", dataset = "mmusculus_gene_ensembl")
 )
 
-# Replace empty values with NA
-biomart_id_mapping_mouse_2 <- biomart_id_mapping_mouse_1 %>%
-  replace(. == "", NA)
-
-# Keep only one row for each Ensembl gene
-biomart_id_mapping_mouse <- biomart_id_mapping_mouse_2 %>%
+biomart_id_mapping_mouse <- biomart_id_mapping_mouse_0 %>%
+  as_tibble() %>%
   rename("entrez_gene_id" = entrezgene_id) %>%
-  mutate(across(everything(), as.character)) %>%
+  mutate(
+    across(everything(), as.character),
+    across(everything(), ~str_replace(.x, "^$", NA_character_))
+  ) %>%
   arrange(ensembl_gene_id, mgi_symbol, entrez_gene_id) %>%
-  distinct() %>%
-  as_tibble()
+  distinct()
 
 # Save the data for use in the package
 usethis::use_data(biomart_id_mapping_mouse, overwrite = TRUE)
