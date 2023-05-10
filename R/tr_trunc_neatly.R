10259ee5dde98a9efb902d50640f2153e3ec26e5
Travis Blimkie <travis.m.blimkie@gmail.com>
2023-05-10 11:22:50 -0700

    Updated tr_trunc_neatly so it should no longer need to be wrapped inside map_chr - its internal now.

--- R/tr_trunc_neatly.R
+++ R/tr_trunc_neatly.R
@@ -1,32 +1,37 @@
-#' tr_trunc_neatly
+#' Cleanly trim truncate long strings
 #'
 #' @param x Input string to trim to desired length, appending an ellipsis to the
 #'   end, and without splitting words
 #' @param l Desired length at which to trim strings
 #'
-#' @return A character string, trimmed to the desired length and with an
-#'   ellipsis at the end
+#' @return Character
 #'
 #' @export
 #'
 #' @import stringr
+#' @importFrom purrr map_chr
 #'
 #' @description Trims the input string to the desired length, appending an
-#'   ellipsis to the end, without splitting in the middle of a word
+#'   ellipsis to the end, without splitting in the middle of a word.
 #'
 #' @references None.
 #'
 #' @seealso <https://www.github.com/travis-m-blimkie/tRavis>
 #'
 tr_trunc_neatly <- function(x, l = 60) {
-
-  if (str_length(x) <= l) {
-    return(x)
-  } else {
-    shortened <- x %>%
-      as.character() %>%
-      str_sub(., start = 1, end = l) %>%
-      str_replace(., pattern = "\\s([^\\s]*)$", replacement = "...")
-    return(shortened)
-  }
+  map_chr(
+    x,
+    ~if (is.na(.x)) {
+      return(NA_character_)
+    } else if (str_length(.x) <= l) {
+      return(.x)
+    } else {
+      shortened <- .x %>%
+        as.character() %>%
+        str_sub(., start = 1, end = l) %>%
+        str_replace(., pattern = "\\s([^\\s]*)$", replacement = "...")
+      return(shortened)
+    }
+  )
 }
+
