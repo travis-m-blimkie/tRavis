c683a952f119340f8aa33aa38cc378f5074dcb3e
Travis Blimkie <travis.m.blimkie@gmail.com>
2024-03-11 15:45:56 -0700

    Fixed test, using data.frame instead of tibble.

--- tests/testthat/test-get_rounded_max.R
+++ tests/testthat/test-get_rounded_max.R
@@ -1,7 +1,7 @@
 test_that("the function is working", {
   set.seed(1)
 
-  count_table <- tibble(
+  count_table <- data.frame(
     sample = rep(paste0("s", seq(1, 5)), each = 2),
     read_type = rep(c("unique", "duplicate"), 5),
     n_reads = rnorm(n = 10, mean = 20e6, sd = 5e6)
