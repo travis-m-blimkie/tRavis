6c670922b2fa68540a3ba9a32b1fb1da489635d8
Travis Blimkie <travis.m.blimkie@gmail.com>
2024-06-04 15:38:57 -0700

    Added an extra check and test to get_rounded_max.

--- tests/testthat/test-get_rounded_max.R
+++ tests/testthat/test-get_rounded_max.R
@@ -9,3 +9,14 @@ test_that("the function is working", {
 
   expect_equal(get_rounded_max(count_table), 6e7)
 })
+
+test_that("missing columns are handled properly", {
+  set.seed(1)
+
+  count_table <- data.frame(
+    sample = rep(paste0("s", seq(1, 5)), each = 2),
+    read_type = rep(c("unique", "duplicate"), 5)
+  )
+
+  expect_error(get_rounded_max(count_table))
+})
